c6c644a6db5b4df80c7a78c856222e9b898f8bb8
Amir Hajipoor <amir.hjr@outlook.com>
2024-08-28 14:51:19 +0330

    Fix github homepage link issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Layout>
       <div className='p-6 text-slate-700 leading-8'>
-        <p>Welcome to this webapp. A react.js project written by Amirhossein Hajipoor & Alireza Asadollahi. <a className='underline text-teal-600' href="github.com/amirhajipoor/technical-english">GitHub</a></p>
+        <p>Welcome to this webapp. A react.js project written by Amirhossein Hajipoor & Alireza Asadollahi. <a className='underline text-teal-600' href="https://github.com/amirhajipoor/technical-english">GitHub</a></p>
         <p>Teacher dr. Shirmohammadi</p>
         <p>Shahid Rajaee University</p>
       </div>
